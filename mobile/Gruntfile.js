dac3fe0f12b396b30f30209660d8a07453af49ef
saferthanhouses <joey_oliver@hotmail.com>
2015-11-12 18:54:06 -0500

    merge conf

--- mobile/Gruntfile.js
+++ mobile/Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
         constants: {
           ENV: {
             name: 'development',
-            apiEndpoint: 'http://dev.yoursite.com:10000/'
+            apiEndpoint: 'http://192.168.3.96:1337/'
           }
         }
       },
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
         constants: {
           ENV: {
             name: 'production',
-            apiEndpoint: 'http://api.yoursite.com/'
+            apiEndpoint: 'http://192.168.3.96:1337/'
           }
         }
       }
